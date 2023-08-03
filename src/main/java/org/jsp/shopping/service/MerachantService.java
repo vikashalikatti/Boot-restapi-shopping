518a2b6f886b4077d6c408a46f29ecb0b5f450f9
vikashalikatti <vikaslahalikatti117@gmail.com>
2023-08-03 23:57:57 +0530

    shopping

--- src/main/java/org/jsp/shopping/service/MerachantService.java
+++ src/main/java/org/jsp/shopping/service/MerachantService.java
@@ -3,13 +3,23 @@ package org.jsp.shopping.service;
 import java.io.IOException;
 
 import org.jsp.shopping.dto.Merchant;
+import org.jsp.shopping.dto.Product;
 import org.jsp.shopping.helper.ResponseStructure;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.http.HttpSession;
 
 public interface MerachantService {
 	public ResponseEntity<ResponseStructure<Merchant>> signup(Merchant merchant, String date, MultipartFile pic)
 			throws IOException;
 
 	public ResponseEntity<ResponseStructure<Merchant>> verifyOtp(String email, int otp);
+
+	public ResponseEntity<ResponseStructure<Merchant>> resendOtp(String email);
+
+	public ResponseEntity<ResponseStructure<Merchant>> login(String email, String password, HttpSession session);
+
+	public ResponseEntity<ResponseStructure<Merchant>> addProduct(HttpSession session, Product product,
+			MultipartFile pic) throws IOException;
 }
